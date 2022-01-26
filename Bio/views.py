9472454f6f35fc2b7619b6884a57be8083f7a99e
Abhijeet <mi01071997gmail.com>
2022-01-26 16:24:44 +0530

    Production

--- Bio/views.py
+++ Bio/views.py
@@ -5,11 +5,14 @@ from Bio.models import User
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    user=User.objects.all()
-
+    data=User.objects.all()
+    #for user in data:
+    #    print(user.first_name,user.last_name,user.address)
+    #args={'user':user}
 
     #user={'first_name':"Abhi",'last_name':"jeet"}
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # context = {'latest_question_list': latest_question_list}
     #return render(request, 'index.html',{'user':user})
-    return render(request, 'index.html',{'user':user})
+
+    return render(request, 'index.html',{'data':data})
