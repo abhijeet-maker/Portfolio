ca6b66217b4e732f84a6d1c8e2dfe7524f8b9aee
Abhijeet <mi01071997gmail.com>
2022-01-26 17:34:37 +0530

    Production

--- Bio/models.py
+++ Bio/models.py
@@ -1,7 +1,7 @@
 import textwrap
 
 from django.db import models
-
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class User(models.Model):
@@ -9,10 +9,13 @@ class User(models.Model):
     first_name=models.CharField(max_length=200,default=None)
     last_name=models.CharField(max_length=200,default=None)
     address=models.CharField(max_length=200,default=None)
-    phone=models.IntegerField(default=None)
+    phoneNumberRegex = RegexValidator(regex=r"^\+?91?\d{8,15}$")
+    phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
+    #phone=models.IntegerField(default=None)
     mail=models.CharField(max_length=30,default=None)
     prof_img=models.ImageField(upload_to='profile_pic/',default=None)
     about=models.TextField(max_length=500,default=None)
+
     def __str__(self):
         name=self.first_name
         #email=self.prof_img
