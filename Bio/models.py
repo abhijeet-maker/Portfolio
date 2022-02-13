bca8ff827c56b1bc35966b2eb3cdde82b3e8285d
Abhijeet <mi01071997gmail.com>
2022-02-13 18:20:33 +0530

    Production

--- Bio/models.py
+++ Bio/models.py
@@ -16,7 +16,29 @@ class User(models.Model):
     prof_img = models.ImageField(upload_to='profile_pic/', default=None)
     about = models.TextField(max_length=500, default=None)
 
-    def __str__(self):
-        name = self.first_name
-        # email=self.prof_img
-        return name
+    #College
+
+    university=models.TextField(max_length=500, default=None)
+    degree=models.TextField(max_length=20, default=None)
+    degree_specialisations=models.TextField(max_length=50, default=None)
+    degree_cgpa=models.DecimalField(max_digits=3,decimal_places=1)
+    degree_date=models.DateField()
+
+    # Intermediate School Detail
+
+    intermediate=models.TextField(max_length=500, default=None)
+    intermediate_specialisations=models.TextField(max_length=50, default=None)
+    intermediate_percentage=models.DecimalField(max_digits=4,decimal_places=2)
+    intermediate_date=models.DateField()
+
+    #School Detail
+    matriculation=models.TextField(max_length=500, default=None)
+    #matriculation_specialisations=models.TextField(max_length=500, default=None)
+    matriculation_percentage=models.DecimalField(default=8.8,max_digits=2,decimal_places=1)
+    matriculation_date=models.DateField()
+
+
+def __str__(self):
+    name = self.first_name
+    # email=self.prof_img
+    return name
