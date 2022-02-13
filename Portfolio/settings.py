cdaa815a02e4102e8c8bf35b1df240259dc2d56d
Abhijeet <mi01071997gmail.com>
2022-02-13 19:49:11 +0530

    Production

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wwbuc1__+r55wcbedhuv8klb(625h*5c4m1(gzc@nc6r%byakj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['myfolioabhi01.herokuapp.com','*', 'localhost', '127.0.0.1']
@@ -129,8 +129,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "download_raw")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
