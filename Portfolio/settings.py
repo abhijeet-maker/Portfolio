33534c0b61660b72e23a7457cc018883fd5009b9
Abhinav Kumar <mi01071997@gmail.com.com>
2023-10-13 00:13:31 +0530

    p2

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'admin',
